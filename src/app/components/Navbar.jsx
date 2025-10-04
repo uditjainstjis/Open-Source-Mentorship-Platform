185c1f42776c367a78c4531545f536d50546b6f0
Udit Jain <uditjainstjis@gmail.com>
2025-10-05 05:21:42 +0530

    logout feat made

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,5 +1,7 @@
+'use client';
 import React from "react";
-import { Search } from "lucide-react"; // install via: npm install lucide-react
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Search } from "lucide-react";
 
 export default function Navbar() {
   return (
@@ -18,22 +20,42 @@ export default function Navbar() {
           />
         </div>
 
-        {/* Profile Icon */}
-        <div className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-            className="w-6 h-6 text-gray-500"
-          >
-            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-            <path
-              fillRule="evenodd"
-              d="M14 14s-1-1.5-6-1.5S2 14 2 14s1-4 6-4 6 4 6 4z"
+        {/* Profile / Auth Section */}
+        <div className="flex items-center justify-center">
+          {/* If signed out, show sign-in button */}
+          <SignedOut>
+            <SignInButton mode="modal">
+              <button className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center hover:bg-gray-300 transition">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                  className="w-6 h-6 text-gray-500"
+                >
+                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M14 14s-1-1.5-6-1.5S2 14 2 14s1-4 6-4 6 4 6 4z"
+                  />
+                </svg>
+              </button>
+            </SignInButton>
+          </SignedOut>
+
+          {/* If signed in, show Clerk user button (menu has Sign out option) */}
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  userButtonAvatarBox: "w-10 h-10",
+                },
+              }}
+              afterSignOutUrl="/"
             />
-          </svg>
+          </SignedIn>
         </div>
       </div>
+
       {/* Thin Blue Top Border */}
       <div className="h-[3px] bg-blue-500 w-full" />
     </nav>
